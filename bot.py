794717ea11583fc4f1f11a91e0657586a435eb4c
MeaZea <maezzmea@gmail.com>
2024-10-17 12:50:25 +0700

    обавлена функция сохранения всех поступающих сообщений

--- bot.py
+++ bot.py
@@ -12,15 +12,18 @@ API_KEY = os.getenv("API_KEY")
 
 bot = telebot.TeleBot(TOKEN)
 
+def save_message(message):
+    with open("messages.log", "a", encoding="utf-8") as file:
+        file.write(f"{message.from_user.id}: {message.text}\n")
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    save_message(message)
     if message.text == '/weather':
         bot.send_message(message.from_user.id, 'Введите название города')
         bot.register_next_step_handler(message, get_weather)
     else:
         bot.send_message(message.from_user.id, "Напиши /weather чтобы узнать погоду")
-
 
 @bot.message_handler(commands=['url'])
 def send_url(message):
@@ -29,11 +32,9 @@ def send_url(message):
     markup.add(btn_my_site)
     bot.send_message(message.chat.id, "Всегда актуальный прогноз для Минска", reply_markup=markup)
 
-
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     bot.send_message(message.chat.id, "Привет! Я бот, который может помочь узнать погоду.")
-
 
 def get_weather(message):
     city = message.text
@@ -50,11 +51,9 @@ def get_weather(message):
     except Exception as e:
         bot.send_message(message.from_user.id, f'Произошла ошибка: {str(e)}')
 
-
 def get_location(lat, lon):
     url = f"https://yandex.ru/pogoda/maps/nowcast?lat={lat}&lon={lon}&via=hnav&le_Lighting=1"
     return url
-
 
 def weather(city: str):
     owm = OWM(API_KEY)
@@ -64,7 +63,6 @@ def weather(city: str):
     location = get_location(observation.location.lat, observation.location.lon)
     temperature = current_weather.temperature("celsius")
     return temperature, location
-
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0)
